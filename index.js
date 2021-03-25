bbb45b2d1daa4418b294f1e382e50f626d9bb8b6
Jelmer de Waal <jelmer.dewaal@paireducation.nl>
2021-03-25 16:00:33 +0100

    Preserve headers

--- index.js
+++ index.js
@@ -1,13 +1,15 @@
-const fetch = require('make-fetch-happen');
+const fetch = require("make-fetch-happen");
 
-const makeDriver = opts => {
+const makeDriver = (opts) => {
   const driverFetch = fetch.defaults(opts);
 
   return (ctx, cb) => {
-    driverFetch(ctx.url, { headers: ctx.headers })
-      .then(res => res.text())
-      .then(body => cb(null, body))
-      .catch(err => cb(err, null));
+    driverFetch(ctx.url, {
+      headers: { ...(opts.headers || {}), ...(ctx.headers || {}) },
+    })
+      .then((res) => res.text())
+      .then((body) => cb(null, body))
+      .catch((err) => cb(err, null));
   };
 };
 
